bed17d25d4b3c46e9cc7700bbbea9d26a18d66c4
nikkishkin <nikkishkin@ya.ru>
2018-07-13 01:28:52 +0300

    Added SignOut functionality and Url.Action for HTML forms

--- DrawMap/Controllers/HomeController.cs
+++ DrawMap/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DrawMap.Models;
 
 namespace DrawMap.Controllers
 {
@@ -14,17 +15,10 @@ namespace DrawMap.Controllers
         {
             using (DrawMapModel context = new DrawMapModel())
             {
-                HttpCookie authorizationCookie = Request.Cookies.Get("authorization");
-
-                if (authorizationCookie == null)
-                {
-                    return View("Login");
-                }
-
                 User currentUser = GetCurrentUser();
-                if (currentUser == null)
+                if (currentUser == null || !UserHasLoggedIn(currentUser.Id))
                 {
-                    return View("Error");
+                    return View("Login", new LogInModel());
                 }
 
                 return View(context.Notes.Where(n => n.UserId == currentUser.Id).ToList());
@@ -41,8 +35,121 @@ namespace DrawMap.Controllers
                     return View("ViewMap", note);
                 }
 
+                ViewBag.ErrorMessage = "Note with id = '" + noteId + "' does not exist";
                 return View("Error");
             }
+        }
+
+        public ActionResult AddNote(string name, string content)
+        {
+            User currentUser = GetCurrentUser();
+            if (currentUser == null || !UserHasLoggedIn(currentUser.Id))
+            {
+                return View("Login", new LogInModel());
+            }
+
+            using (DrawMapModel context = new DrawMapModel())
+            {
+                context.Notes.Add(new Note {Name = name, Content = content, UserId = currentUser.Id});
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RenameNote(int noteId, string name)
+        {
+            User currentUser = GetCurrentUser();
+            if (currentUser == null || !UserHasLoggedIn(currentUser.Id))
+            {
+                return View("LogIn", new LogInModel());
+            }
+
+            using (DrawMapModel context = new DrawMapModel())
+            {
+                Note note = context.Notes.SingleOrDefault(n => n.Id == noteId);
+                if (note == null || note.UserId != currentUser.Id)
+                {
+                    ViewBag.ErrorMessage = "Note with id = '" + noteId +
+                                           "' does not exist or it is unavailable for the current user";
+                    return View("Error");
+                }
+
+                note.Name = name;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult DeleteNote(int noteId)
+        {
+            User currentUser = GetCurrentUser();
+            if (currentUser == null || !UserHasLoggedIn(currentUser.Id))
+            {
+                return View("LogIn", new LogInModel());
+            }
+
+            using (DrawMapModel context = new DrawMapModel())
+            {
+                Note note = context.Notes.SingleOrDefault(n => n.Id == noteId);
+                if (note == null || note.UserId != currentUser.Id)
+                {
+                    ViewBag.ErrorMessage = "Note with id = '" + noteId +
+                                           "' does not exist or it is unavailable for the current user";
+                    return View("Error");
+                }
+
+                context.Notes.Remove(note);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult LogIn(string email, string password)
+        {
+            using (DrawMapModel context = new DrawMapModel())
+            {
+                User dbUser = context.Users.SingleOrDefault(u => u.Email == email);
+
+                if (dbUser == null)
+                {
+                    dbUser = new User {Email = email, Password = password};
+                    context.Users.Add(dbUser);
+                    context.SaveChanges();
+                }
+                else if (dbUser.Password != password)
+                {
+                    return View("Login",
+                        new LogInModel
+                        {
+                            HasError = true,
+                            ErrorMessage = "User with the same login exists, but the password is incorrect"
+                        });
+                }
+
+                Response.Cookies.Add(new HttpCookie("authorization", String.Concat(email, " - ", password)));
+                Session.Add(dbUser.Id.ToString(), true);
+
+                return RedirectToAction("Index");             
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            User currentUser = GetCurrentUser();
+            if (currentUser != null)
+            {
+                Session[currentUser.Id.ToString()] = false;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private bool UserHasLoggedIn(int userId)
+        {
+            return (Session[userId.ToString()] != null) && (bool)Session[userId.ToString()];
         }
 
         private User GetCurrentUser()
@@ -62,95 +169,6 @@ namespace DrawMap.Controllers
             using (DrawMapModel context = new DrawMapModel())
             {
                 return context.Users.SingleOrDefault(u => u.Email == email && u.Password == password);
-            }
-        }
-
-        //private ActionResult ReturnErrorView(string message)
-        //{
-        //    return 
-        //}
-
-        public ActionResult AddNote(string name, string content)
-        {
-            User currentUser = GetCurrentUser();
-            if (currentUser == null)
-            {
-                return View("Error");
-            }
-
-            using (DrawMapModel context = new DrawMapModel())
-            {
-                context.Notes.Add(new Note {Name = name, Content = content, UserId = currentUser.Id});
-                context.SaveChanges();
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult RenameNote(int noteId, string name)
-        {
-            User currentUser = GetCurrentUser();
-            if (currentUser == null)
-            {
-                return View("Error");
-            }
-
-            using (DrawMapModel context = new DrawMapModel())
-            {
-                Note note = context.Notes.SingleOrDefault(n => n.Id == noteId);
-                if (note == null || note.UserId != currentUser.Id)
-                {
-                    return View("Error");
-                }
-
-                note.Name = name;
-                context.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-        }
-
-        public ActionResult DeleteNote(int noteId)
-        {
-            User currentUser = GetCurrentUser();
-            if (currentUser == null)
-            {
-                return View("Error");
-            }
-
-            using (DrawMapModel context = new DrawMapModel())
-            {
-                Note note = context.Notes.SingleOrDefault(n => n.Id == noteId);
-                if (note == null || note.UserId != currentUser.Id)
-                {
-                    return View("Error");
-                }
-
-                context.Notes.Remove(note);
-                context.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-        }
-
-        public ActionResult LogIn(string email, string password)
-        {
-            using (DrawMapModel context = new DrawMapModel())
-            {
-                User dbUser = context.Users.SingleOrDefault(u => u.Email == email);
-
-                if (dbUser == null)
-                {
-                    context.Users.Add(new User { Email = email, Password = password });
-                    context.SaveChanges();
-                }
-                else if (dbUser.Password != password)
-                {
-                    return View("Error");
-                }
-
-                Response.Cookies.Add(new HttpCookie("authorization", String.Concat(email, " - ", password)));
-                return RedirectToAction("Index");             
             }
         }
     }
